1f1311f1250f05f2a3c0700feeac98dd4135fa92
muss3ab <muss3ab.1994@gmail.com>
2023-08-16 21:22:37 +0300

    Add bootstrap && set folder structure && add user auth package

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,16 @@
+<!-- /.content-wrapper -->
+<footer class="main-footer">
+    <strong>Copyright &copy; 2023 </strong>
+    All rights reserved For Arab Workers
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 0.0.1
+    </div>
+  </footer>
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
